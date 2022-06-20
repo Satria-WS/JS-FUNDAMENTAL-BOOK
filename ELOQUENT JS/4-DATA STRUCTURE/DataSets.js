ea731df32a08e2ffab2a705748d2ce4778669853
Satria-WS <extencezone02@gmail.com>
2022-06-21 03:51:14 +0700

    wakeup
    wakeup

--- ELOQUENT JS/4-DATA STRUCTURE/DataSets.js
+++ ELOQUENT JS/4-DATA STRUCTURE/DataSets.js
@@ -124,7 +124,31 @@ function phi(table) {
 console.log(phi([76, 9, 4, 1]));
 // → 0.068599434
 
+
+//var object journal
+var JOURNAL = [
+  { "events": ["carrot", "exercise", "weekend"], "squirrel": false },
+  { "events": ["bread", "pudding", "brushed teeth", "weekend", "touched tree"], "squirrel": false }
+]
+
 //# Modern javascript loop
 for (let entry of JOURNAL) {
   console.log(`${entry.events.length} events.`);
 }
+
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+console.log(journalEvents(JOURNAL));
+
+
+
+//SKIP.......
