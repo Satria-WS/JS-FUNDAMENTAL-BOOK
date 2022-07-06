3feb291a23c6bbbc57d2a814b780a9333972c620
Satria-WS <extencezone02@gmail.com>
2022-07-06 07:23:51 +0700

    Create Creating_Rexpression.js

---
+++ ELOQUENT JS/9-Regular Expressions/Creating_Rexpression.js
@@ -0,0 +1,27 @@
+//Regular expression is a type of object
+//when using the RegExp constructor , the pattern is written as a normal string
+//using forward slash ends the pattern
+//using backslash to be parts of the pattern
+let re1 = new RegExp(true);
+let re2 = /abcdef/;
+console.log(re1); //abcdef
+console.log(re2); //abcdef
+console.log(typeof re1); //object
+
+//##example1
+let eighteenPlus1 = /eighteen/;
+let eighteenPlus2 = "/eighteen/";
+console.log(typeof eighteenPlus1); //object
+console.log(typeof eighteenPlus2); //string
+
+//##example2
+let eighteenPlus3 = /eighteen\+/;
+console.log(eighteenPlus3);
+
+//# Regular expression methode
+//telling whether a match of the pattern true or false
+let testReg1 = /true/.test(true);
+let testReg2 = /"abc"/.test("abc");
+console.log(typeof testReg1);//true boolean
+console.log(typeof testReg2); //false boolean
+
