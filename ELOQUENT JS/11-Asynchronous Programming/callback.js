5da50ab5d08a084c80bc811b33320960f2087367
Satria-WS <extencezone02@gmail.com>
2022-07-07 16:41:09 +0700

    add prmise

---
+++ ELOQUENT JS/11-Asynchronous Programming/callback.js
@@ -0,0 +1,13 @@
+//Callbackks
+
+setTimeout(() => {
+  console.log("hello world 5 sec");
+}, 5000);
+
+setTimeout(() => {
+  console.log("hello world 2 sec");
+}, 2000);
+
+setTimeout(() => {
+  console.log("hello world 1 sec");
+}, 1000);
---
+++ ELOQUENT JS/11-Asynchronous Programming/promise.js
@@ -0,0 +1,6 @@
+//A promise is an asynchronous action that may complete at some point and produce a value
+//It is useful to think of promises as a device to move values into an asynchronous reality
+let fifteen = Promise.resolve(15);
+fifteen.then((value) => {
+  console.log(`Got ${value}`);
+});
