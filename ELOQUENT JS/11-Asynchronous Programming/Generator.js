74499c53fa75c1d78254b767e4ed077242b924f7
Satria-WS <extencezone02@gmail.com>
2022-07-08 18:03:48 +0700

    add iterator

--- ELOQUENT JS/11-Asynchronous Programming/Generator.js
+++ ELOQUENT JS/11-Asynchronous Programming/Generator.js
@@ -1,6 +1,15 @@
 //Javascript also has a featured called generator function
 
-function* powers(n) {
-    
+function* powers(number) {
+  for (let current = number; ; current += number) {
+    yield current;
+  }
 }
+
+// console.log(powers(10).next())
+
+let powerRanger = powers(1);
+console.log(powerRanger.next());
+console.log(powerRanger.next());
+console.log(powerRanger.next());
 
---
+++ ELOQUENT JS/11-Asynchronous Programming/forOf.js
@@ -0,0 +1,17 @@
+//##Iterator on array
+let cityName = ["bogor", "depok", "citayam"];
+for (kota of cityName) {
+  console.log(kota);
+}
+
+const iterable = [10, 20, 30];
+
+for (let value of iterable) {
+  value += 1;
+  console.log(value);
+}
+
+
+
+
+//## Iterator on object
