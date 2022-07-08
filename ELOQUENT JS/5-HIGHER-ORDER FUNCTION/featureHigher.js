f869fcdd12e1a1af9fc5fbc5e8a52ed86b29a014
Satria-WS <extencezone02@gmail.com>
2022-07-08 10:22:52 +0700

    Higher order function

---
+++ ELOQUENT JS/5-HIGHER-ORDER FUNCTION/featureHigher.js
@@ -0,0 +1,17 @@
+//##Kind of Higher order function
+
+//##Array.map()
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let add = (value) => {
+  return value ;
+};
+let copy = numbers.map(add);
+// console.log(copy);
+
+//##Similar oldway
+for(let i = 0; ; i++) {
+   // console.log(numbers)
+}
+
+
+//refrence: https://gading.dev/id/blog/mengenal-higher-order-function-di-javascript
