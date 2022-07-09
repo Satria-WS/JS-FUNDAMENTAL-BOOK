d20779acada6cc3c604264722e060c3cdcc674e3
Satria-WS <extencezone02@gmail.com>
2022-07-09 09:53:15 +0700

    Update eventDom.js

--- ELOQUENT JS/15-Handling Events/eventDom.js
+++ ELOQUENT JS/15-Handling Events/eventDom.js
@@ -5,4 +5,9 @@ button.addEventListener("click", () => {
 });
 
 
-
+var button = document.querySelector("button");
+function once() {
+  console.log("Done.");
+  button.removeEventListener("click", once);
+}
+button.addEventListener("click", once);
